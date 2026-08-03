c5dd73fb37a2f0ef68870962b6af5dc7d56a161c
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-08-03 15:17:20 +0900

    Loadingコンポーネント作成

---
+++ src/components/common/Loading/Loading.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Loading from "./Loading";
+
+const meta: Meta<typeof Loading> = {
+  title: "Common/Loading",
+  component: Loading,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Loading>;
+
+export const Default: Story = {
+  args: {
+    open: true,
+  },
+};
